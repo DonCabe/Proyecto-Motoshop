f18cfded89b36cad8a443987cf1ae6a90e753d24
unknown <eperespicaro@gmail.com>
2024-04-07 22:08:23 -0300

    Se agregó funcionalidad a datos de contacto y se mejoró el carrito para que muestre mejor la información.

--- js/carrito.js
+++ js/carrito.js
@@ -3,14 +3,49 @@ cartStorage = JSON.parse(cartStorage)
 
 let cartContainer = document.getElementById("cart-selection")
 
-function renderCarrito (cartItems) {
-    cartItems.forEach(producto => {
-        const cart = document.createElement("div")
-        cart.innerHTML = `<h3>${producto.producto}</h3>
-                          <p>$${producto.precio}</p>
-                          <br>`
-        cartContainer.appendChild(cart)
-    });
+function renderCarrito (cartItems) {    //Función para renderizar los items del carrito
+    if (cartItems) {
+        cartItems.forEach(producto => {
+            const cart = document.createElement("div")
+            cart.innerHTML = `<h3>${producto.producto}</h3>
+                            <p>$${producto.precio}</p>
+                            <button class="eliminar">eliminar</button>
+                            <br>`
+            cartContainer.appendChild(cart)
+        });
+    }
 }
 
-renderCarrito(cartStorage)
+function cuenta (cartItems) {
+    const cart = document.querySelector("#cuenta")
+    if (cartItems){
+        const total = cartItems.reduce((cont, producto) => cont+producto.precio,0)
+        cart.innerHTML = `<h2>Total: $${total}</h2>
+                          <button class="vaciar">Vaciar Carrito</button>`
+    }else {
+        cart.innerHTML = `<h4>El carrito está vacio.</h4>`;
+    }
+}
+
+const eliminarProducto = (id) => {
+    const idProducto = cartStorage.find((producto) => producto.id === id);  //busco el elemento del carrito clickeado
+    console.log(idProducto);
+    
+    const nuevoCarrito = cartStorage.filter((producto) => producto.id !== idProducto);   //filtro todos los elementos del carrito que no sean el del id clickeado
+    console.log(nuevoCarrito);
+
+    cartStorage = nuevoCarrito;     //le asigno el valor de nuevoCarrito al carrito
+}
+
+const removeButton = cartContainer.querySelectorAll(".eliminar")
+removeButton.onclick = () => {
+    eliminarProducto(producto.id);
+    
+    renderCarrito(cartStorage);
+    console.log();
+    console.log("Item Eliminado");
+}
+
+renderCarrito(cartStorage);
+cuenta(cartStorage);
+console.log(cartStorage);
--- js/main.js
+++ js/main.js
@@ -4,90 +4,116 @@ const dolarHoy = 1010
 
 class Moto {
     static id = 0
-    constructor (marca, modelo, precio) {
+    constructor (marca, modelo, precio, img) {
         this.id = ++Moto.id,
         this.producto = marca + " - " + modelo,
-        this.precio = precio * dolarHoy
+        this.precio = precio * dolarHoy,
+        this.img = img
     }
 };
 
 // class Accesorio {
 //     static id = 0
-//     constructor (producto, precio)
+//     constructor (producto, precio, img)
 //     {
 //         this.id = ++Accesorio.id,
 //         this.producto = producto
-//         this.precio = precio * dolarHoy
+//         this.precio = precio * dolarHoy,
+//         this.img = img
 //     }
 // };
+
+const datosContacto = {
+    nombre: "Martín Perez",
+    telefono: "11-2345-6789",
+    correo: "mperez.motoshop@gmail.com"
+};
 
 patentamiento = (precio) => {   //función para calcular el costo del patentamiento de la moto
     precio = precio * 0.09      //el costo del patentamiento es un 9% del valor en pesos
     return precio
 };
 
-const moto1 = new Moto ('Honda','Wave',1700)
-const moto2 = new Moto ('Honda','XR250',5500)
-const moto3 = new Moto ('Yamaha','FZ X',2400)
-const moto4 = new Moto ('Yamaha','YBR125',2600)
-const moto5 = new Moto ('Suzuki','GN125',2600)
-const moto6 = new Moto ('Suzuki','GSX125',2600)
+const moto1 = new Moto ('Honda','Wave',1700,"");
+const moto2 = new Moto ('Honda','XR250',5500,"");
+const moto3 = new Moto ('Yamaha','FZ X',2400,"");
+const moto4 = new Moto ('Yamaha','YBR125',2600,"");
+const moto5 = new Moto ('Suzuki','GN125',2600,"");
+const moto6 = new Moto ('Suzuki','GSX125',2600,"");
 
 const motos = [moto1, moto2, moto3, moto4, moto5, moto6];
 
-// const accesorio1 = new Accesorio ('Casco Hawk',50)
-// const accesorio2 = new Accesorio ('Casco LS2',150)
-// const accesorio3 = new Accesorio ('Baulera',70)
-// const accesorio4 = new Accesorio ('Piloto Lluvia',30)
-// const accesorio5 = new Accesorio ('Campera Ref.',300)
-// const accesorio6 = new Accesorio ('Guantes Ref.',30)
-// const accesorio7 = new Accesorio ('Cadena',15)
-// const accesorio8 = new Accesorio ('Traba Disco',30)
+// const accesorio1 = new Accesorio ('Casco Hawk',50,"");
+// const accesorio2 = new Accesorio ('Casco LS2',150,"");
+// const accesorio3 = new Accesorio ('Baulera',70,"");
+// const accesorio4 = new Accesorio ('Piloto Lluvia',30,"");
+// const accesorio5 = new Accesorio ('Campera Ref.',300,"");
+// const accesorio6 = new Accesorio ('Guantes Ref.',30,"");
+// const accesorio7 = new Accesorio ('Cadena',15,"");
+// const accesorio8 = new Accesorio ('Traba Disco',30,"");
 
 // const accesorios = [accesorio1, accesorio2, accesorio3, accesorio4, accesorio5, accesorio6, accesorio7, accesorio8];
 
+
+//muestra los accesorios
 let motocicletasMenu = document.getElementById("motocicletas")
 motocicletasMenu.onclick = () => {
-    renderMotos(motos)
+    renderProductos(motos);
 }
 
+//muestra los accesorios
 let accesoriosMenu = document.getElementById("accesorios")
 accesoriosMenu.onclick = () => {
-    alert("Proximamente.")      //aún no me dió el tiempo
+    // renderProductos(accesorios);
+    alert("Próximamente.")
 }
 
-let cartProducts
-let cartProductsLS = localStorage.getItem("cartProducts")  //inicializo el array del carrito con el contenido del LocalStorage
-if(cartProducts){
-    cartProducts = JSON.parse(cartProductsJS)
+//tarjeta de contacto
+let contactoMenu = document.getElementById("contacto")
+contactoMenu.onclick = () => {
+    let limpiar = document.getElementById("producto-lista");
+    limpiar.innerHTML = "";
+    const card = document.createElement ("div");
+    card.className = "card";
+    card.innerHTML = `<h2>${datosContacto.nombre}</h2>
+                      <h3>Tel.: ${datosContacto.telefono}</h3>
+                      <h3>Mail: ${datosContacto.correo}</h3>`        
+    productsContainer.appendChild(card)
+}
+
+
+let cartProducts = [];
+let cartProductsLS = localStorage.getItem("cartProducts");  //inicializo el array del carrito con el contenido del LocalStorage
+if(cartProductsLS){
+    cartProducts = JSON.parse(cartProductsLS);
 } else {
-    cartProducts = []
+    cartProducts = [];
 }
 
-let productsContainer = document.getElementById("producto-lista")   //
+let productsContainer = document.getElementById("producto-lista");   //
 
-function renderMotos(productsArray) {    //Función para renderizar las motos
+function renderProductos(productsArray) {    //Función para renderizar las motos o accesorios
+    let limpiar = document.getElementById("producto-lista");
+    limpiar.innerHTML = "";
     productsArray.forEach (producto => {
-        const card = document.createElement ("div")
-        card.innerHTML = `<div class="producto">
-                          <img src="FotoMoto.jpg" alt="FotoMoto">
+        const card = document.createElement ("div");
+        card.className = "card";
+        card.innerHTML = `<img src="${producto.img}" alt="img ${producto.producto}">
                           <h2>${producto.producto}</h2>
                           <p>Precio: $${producto.precio}</p>
-                          <p>Patentamiento: $${patentamiento(producto.precio)}</p>
-                          <button class="productoAgregar" id="${producto.id}">Agregar</button>
-                          </div>`        
-        productsContainer.appendChild(card) 
+                          <button class="productoAgregar" id="${producto.id}">Agregar</button>`
+        productsContainer.appendChild(card)
     });
-    AddToCartButton(productsArray)
+    addToCartButton(productsArray)
 };
 
 //Función para agregar items de un array(lista) al carrito
-function AddToCartButton () {
+function addToCartButton (producto) {
     let addButton = document.querySelectorAll(".productoAgregar")
     addButton.forEach (button => {
         button.onclick = (e) => {
             const productId = e.currentTarget.id
-            const selectedProduct = motos.find (producto => producto.id == productId)
+            const selectedProduct = producto.find (producto => producto.id == productId)
             cartProducts.push(selectedProduct)
             console.log(cartProducts)
 
@@ -96,4 +122,4 @@ function AddToCartButton () {
     })
 };
 
-console.log('Gracias por utilizar Motoshop.');  //saludo cordial
+console.log('Gracias por utilizar Motoshop.');      //saludo cordial
