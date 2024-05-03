ef30fb578d49232fb614d308229a41ada69a2b89
unknown <eperespicaro@gmail.com>
2024-05-03 05:15:36 -0300

    Se corrigieron errores en las funciones principales, agregado de validación de errores y estilado de la página.

--- js/carrito.js
+++ js/carrito.js
@@ -4,6 +4,12 @@ if(cartProductsLS){         //confirmo que no sea null
     cartProducts = JSON.parse(cartProductsLS);
 }
 
+let datosCompra = "";      //inicializo el carrito
+let datosCompraLS = localStorage.getItem("datosCompra");  //inicializo el array  con el contenido del LocalStorage
+if(datosCompraLS){         //confirmo que no sea null
+    datosCompra = JSON.parse(cartProductsLS);
+}
+
 let cartContainer = document.getElementById("cart-selection")
 
 renderCarrito = () => {    //Función para renderizar los items del carrito
@@ -13,7 +19,7 @@ renderCarrito = () => {    //Función para renderizar los items del carrito
         cartProducts.forEach(producto => {
             const cart = document.createElement("div")
             cart.className = "cart-item";
-            cart.innerHTML = `<!-- <img src="${producto.img}" alt="img ${producto.producto}"> -->
+            cart.innerHTML = `<img src="${producto.img}" alt="img ${producto.producto}">
                               <h3>${producto.producto}</h3>
                               <p>$${producto.precio * producto.cantidad}</p>
                               <span id="cantidad"> Cantidad: 
@@ -36,7 +42,7 @@ renderCarrito = () => {    //Función para renderizar los items del carrito
             restar.onclick = () => {
                 restarProducto(producto.id)
             }
-        });
+        })
     //renderización de la cuenta
     let total = cartProducts.reduce((cont, producto) => cont+(producto.precio * producto.cantidad),0)  //calculo el total
 
@@ -49,10 +55,58 @@ renderCarrito = () => {    //Función para renderizar los items del carrito
         vaciarCarrito();
     }
     const finCompra = document.querySelector("#fin-compra")
-    finCompra.onclick = () => {
-        
+        finCompra.onclick = async () => {
+        const { value: formValues } = await Swal.fire({
+            title: "Desea finalizar la compra?",
+            html: `
+                <h4>Total: $${total}</h4>
+                <p>Ingrese sus datos de contacto</p>
+                <label for="swal-input1">Nombre</label>
+                <input id="swal-input1" class="swal2-input">
+                <label for="swal-input2">Apellido</label>
+                <input id="swal-input2" class="swal2-input">
+                <label for="swal-input3">Email</label>
+                <input id="swal-input3" class="swal2-input">
+                `,
+            showCancelButton: true,
+            confirmButtonColor: "#333333",
+            confirmButtonText: "Sí, finalizar compra",
+            cancelButtonColor: "#d33",
+            cancelButtonText: "Cancelar",
+            focusConfirm: false,
+            preConfirm: () => {
+                const name = document.getElementById("swal-input1").value;
+                const surname = document.getElementById("swal-input2").value;
+                const email = document.getElementById("swal-input3").value;
+                const formatoTexto = /^[a-zA-Z- ]+$/
+                if (name == "" || !formatoTexto.test(name)) {
+                    Swal.showValidationMessage("Ingrese un nombre válido");
+                } else if (surname == "" || !formatoTexto.test(surname)) {
+                    Swal.showValidationMessage("Ingrese un apellido válido");
+                } else {
+                    const formatoMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;     // Formato para validad el mail
+                    if (!formatoMail.test(email)) {
+                        Swal.showValidationMessage("Ingrese un email válido");
+                    }
+                }
+                return [name, surname, email];
+            }
+        });
+        if (formValues) {
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "Compra realizada con éxito!",
+                showConfirmButton: false,
+                timer: 1500
+            });
+            datosCompra = JSON.stringify(formValues) + JSON.stringify(cartProducts);
+            localStorage.setItem("datosCompra", JSON.stringify(datosCompra));       //En éste punto se enviarían los detalles de la compra a procesar
+            artProducts = [];       //vacío el carrito
+            localStorage.removeItem("cartProducts");
+            renderCarrito();
+        }
     }
-    
     } else {
         cuenta.innerHTML = `<h2>El carrito está vacio.</h2>`;
     }
@@ -65,39 +119,98 @@ const sumarProducto = (id) => {
     renderCarrito();
 }
 
-const restarProducto = (id) => {
+const restarProducto = async (id) => {
     const productoRestar = cartProducts.find((producto) => producto.id === id);  //busco el elemento del carrito clickeado
     if (productoRestar.cantidad > 1) {
         productoRestar.cantidad--;
         localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
     } else {
-        const nuevoCarrito = cartProducts.filter((producto) => producto.id !== productoRestar.id);   //filtro todos los elementos del carrito que no sean el del id clickeado
-        cartProducts = nuevoCarrito;     //le asigno el valor de nuevoCarrito al carrito
-        localStorage.clear()
-        if (cartProducts.length != 0) {
-            localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
+        await Swal.fire({
+            title: "Desea eliminar el producto?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonText: "Cancelar",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Aceptar"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                const nuevoCarrito = cartProducts.filter((producto) => producto.id !== productoRestar.id);   //filtro todos los elementos del carrito que no sean el del id clickeado
+                cartProducts = nuevoCarrito;     //le asigno el valor de nuevoCarrito al carrito
+                localStorage.clear()
+                if (cartProducts.length != 0) {
+                    localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
+                }
+                Swal.fire({
+                    title: "Producto eliminado",
+                    icon: "success",
+                    showConfirmButton: false,
+                    timer: 750
+                });
+            }
+          });
+    }
+    console.log("Arre")
+    renderCarrito();
+}
+
+const eliminarProducto = async (id) => {      //función para eliminar productos completos
+    await Swal.fire({
+        title: "Desea eliminar el producto?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonText: "Cancelar",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Aceptar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            const idProducto = cartProducts.find((producto) => producto.id === id);
+            const nuevoCarrito = cartProducts.filter((producto) => producto.id !== idProducto.id);   //filtro todos los elementos del carrito que no sean el del id clickeado
+            cartProducts = nuevoCarrito;
+
+            localStorage.clear()
+            if (cartProducts.length != 0) {
+                localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
+            }
+            Swal.fire({
+                title: "Producto eliminado",
+                icon: "success",
+                showConfirmButton: false,
+                timer: 750
+            });
         }
-    }
+      });
     renderCarrito();
 }
 
-const eliminarProducto = (id) => {
-    const idProducto = cartProducts.find((producto) => producto.id === id);  //busco el elemento del carrito clickeado
+const vaciarCarrito = async () => {
+    await Swal.fire({
+        title: "Seguro desea vaciar el carrito?",
+        text: "Si hace esto, deberá cargar todo nuevamente",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonText: "Cancelar",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Aceptar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            cartProducts = [];
+            localStorage.removeItem("cartProducts");
+            renderCarrito();
+            if (cartProducts.length != 0) {
+                localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
+            }
+            Swal.fire({
+                title: "Carrito vacío",
+                icon: "success",
+                showConfirmButton: false,
+                timer: 750
+            });
+        }
+      });
     
-    const nuevoCarrito = cartProducts.filter((producto) => producto.id !== idProducto.id);   //filtro todos los elementos del carrito que no sean el del id clickeado
-    cartProducts = nuevoCarrito;     //le asigno el valor de nuevoCarrito al carrito
-    
-    localStorage.clear()
-    if (cartProducts.length != 0) {
-        localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
-    }
-    renderCarrito();
-}
-
-const vaciarCarrito = () => {
-    cartProducts = [];
-    localStorage.clear();
-    renderCarrito();
 }
 
 renderCarrito();
--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
-const dolarHoy = 1010
-// fetch("/https://dolarapi.com/v1dolares/blue")
-//   .then(response => response.json())
-//   .then(data => console.log(data));
+let dolarHoy = "";
+
+const dolarFunc = async () => {
+    let dolarURL = "https://dolarapi.com/v1/dolares/blue"
+    let valorDolar = "";
+    try {
+        let solicitud = await fetch(dolarURL);
+        let response = await solicitud.json();
+        valorDolar = response.venta;
+    } catch (err) {
+        console.error("Hubo un problema obteniendo el valor del dolar:", err);
+        Swal.fire({
+            icon: "info",
+            title: "Error de respuesta",
+            text: "El valor de los productos será expresado en dólares.",
+            footer: '<a href="./index.html">recargar la página</a>'
+          });
+        valorDolar = 1
+    } finally {
+        dolarHoy = valorDolar;
+    }
+}
+dolarFunc()
 
 enPesos = (precio) => {
     return precio * dolarHoy
 }
-
-const datosContacto = {
-    nombre: "Martín Perez",
-    telefono: "11-2345-6789",
-    email: "mperez.motoshop@gmail.com"
-};
 
 patentamiento = (precio) => {   //función para calcular el costo del patentamiento de la moto
     precio = precio * 0.09      //el costo del patentamiento es un 9% del valor en pesos
@@ -21,34 +34,36 @@ patentamiento = (precio) => {   //función para calcular el costo del patentamie
 const container = document.getElementById("product-container");
 const mainMenu = document.getElementById("menu-principal");
 
-//muestra el menú de inicio
-let inicioMenu = document.getElementById("inicio")
+let inicioMenu = document.getElementById("inicio")   //muestra el menú de inicio
 inicioMenu.onclick = () => {
     renderInicio();
 }
 
-//muestra las motocicletas
-let motocicletasMenu = document.getElementById("motocicletas")
+let motocicletasMenu = document.getElementById("motocicletas")      //muestra las motocicletas
 motocicletasMenu.onclick = () => {
-    renderProductos(true);
+    renderMotos();
 }
 
-//muestra los accesorios
-let accesoriosMenu = document.getElementById("accesorios")
+let accesoriosMenu = document.getElementById("accesorios")      //muestra los accesorios
 accesoriosMenu.onclick = () => {
-    // imagen.src
-    renderProductos(false);
+    renderAccesorios();
 }
 
+let contactoMenu = document.getElementById("contacto")      //tarjeta de contacto
+contactoMenu.onclick = () => {
+    renderContacto();
+}
+
+const itemsCart = document.querySelector("#carrito-counter")
 //Inicializo el carrito y el almacenamiento del mismo
 let cartProducts = [];
 let cartProductsLS = localStorage.getItem("cartProducts");  //inicializo el array del carrito con el contenido del LocalStorage
 if(cartProductsLS){
     cartProducts = JSON.parse(cartProductsLS);
+    itemsCart.textContent = cartProducts.length;
 }
 
-//Inicializo el array de motos que le interesaron al usuario.
-let consultedBikes = [];
+
 
 renderInicio = () => {      //función de renderizado del menú de inicio
     container.innerHTML = "";
@@ -59,70 +74,53 @@ renderInicio = () => {      //función de renderizado del menú de inicio
     titulo.innerHTML = `<h2>Bienvenido a MotoShop</h2>
                         <p>Elija qué quiere buscar con los botones del menú.</p>`;
                         mainMenu.appendChild(titulo)
-    const card = document.createElement ("div");
-    card.className = "ventanas";
-    card.innerHTML = `<span><h3 id="acces">Accesorios</h3></span>
-                      <span><h3 id="motos">Motocicletas</h3></span>`
-    container.appendChild(card)
 }
 
-//tarjeta de contacto
-let contactoMenu = document.getElementById("contacto")
-contactoMenu.onclick = () => {
+renderMotos = () => {     //función para renderizar accesorios
     mainMenu.innerHTML = "";
-    mainMenu.style.backgroundImage = "url('./images/fondos/bg-consulta.jpg')";
     container.innerHTML = "";
-    const card = document.createElement ("div");
-    card.className = "contacto";
-    card.innerHTML = `<h2>${datosContacto.nombre}</h2>
-                      <h3>Tel.: ${datosContacto.telefono}</h3>
-                      <h3>Mail: ${datosContacto.email}</h3>`;
-    container.appendChild(card)
+    mainMenu.style.backgroundImage = "url('./images/fondos/bg-motos.jpg')";
+    fetch('./db/motos.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(producto => {
+            const card = document.createElement ("div");
+            card.className = "card";
+            card.innerHTML = `<h2>${producto.marca}</h2>
+                                <img src="${producto.img}" alt="img ${producto.modelo}">
+                                <h3>${producto.modelo}</h3>
+                                <p>Precio: $${enPesos(producto.precio)}</p>
+                                <button class="consulta" id="${producto.id}">Consultar</button>`
+            container.appendChild(card)
+        })
+        consultButton(data)
+    })
+    .catch(error => {
+        console.log('Error fetching products:', error);
+    });
 }
 
-function renderProductos(bandera) {     //función para renderizar las motos o accesorios
+renderAccesorios = () => {     //función para renderizar motos
     mainMenu.innerHTML = "";
     container.innerHTML = "";
-    if (bandera) {                      //recibe un booleano y en base a eso renderiza motos o accesorios
-        mainMenu.style.backgroundImage = "url('./images/fondos/bg-motos.jpg')";
-        fetch('./db/motos.json')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(producto => {
-                const card = document.createElement ("div");
-                card.className = "card";
-                card.innerHTML = `<h2>${producto.marca}</h2>
-                                  <img src="${producto.img}" alt="img ${producto.modelo}">
-                                  <h3>${producto.modelo}</h3>
-                                  <p>Precio: $${enPesos(producto.precio)}</p>
-                                  <button class="consulta" id="${producto.id}">Consultar</button>`
+    mainMenu.style.backgroundImage = "url('./images/fondos/bg-accesorios.jpg')";
+    fetch('./db/accesorios.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach (producto => {
+            const card = document.createElement ("div");
+            card.className = "card";
+            card.innerHTML = `<img src="${producto.img}" alt="img ${producto.producto}">
+                                <h2>${producto.producto}</h2>
+                                <p>Precio: $${enPesos(producto.precio)}</p>
+                                <button class="productoAgregar" id="${producto.id}">Agregar</button>`
                 container.appendChild(card)
-            })
-            consultButton(data)
-        })
-        .catch(error => {
-            console.log('Error fetching products:', error);
         });
-    } else {
-        mainMenu.style.backgroundImage = "url('./images/fondos/bg-accesorios.jpg')";
-        fetch('./db/accesorios.json')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach (producto => {
-                const card = document.createElement ("div");
-                card.className = "card";
-                card.innerHTML = `<img src="${producto.img}" alt="img ${producto.producto}">
-                                    <h2>${producto.producto}</h2>
-                                    <p>Precio: $${enPesos(producto.precio)}</p>
-                                    <button class="productoAgregar" id="${producto.id}">Agregar</button>`
-                    container.appendChild(card)
-            });
-            addToCartButton(data) 
-        })
-        .catch(error => {
-            console.log('Error fetching products:', error);
-        });
-    }
+        addToCartButton(data)
+    })
+    .catch(error => {
+        console.log('Error fetching products:', error);
+    });
 }
 
 function addToCartButton (producto) {           //Función para agregar items de un array(lista) al carrito
@@ -138,24 +136,117 @@ function addToCartButton (producto) {           //Función para agregar items de
                 selectedProduct.precio = enPesos(selectedProduct.precio)
                 cartProducts.push(selectedProduct);
             }
-            //toastify agregado al carrito!
             localStorage.setItem("cartProducts", JSON.stringify(cartProducts))
+            mensajeAgregado("Añadido al carrito", "./carrito.html")
+            itemsCart.textContent = cartProducts.length
         }
     })
 };
 
+function mensajeAgregado (mensaje, link) {
+    Toastify({
+        text: mensaje,
+        duration: 1500,
+        destination: link,
+        newWindow: false,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+        background: "linear-gradient(to right, #222222, #444444)",
+        fontWeight: "650"
+        },
+        onClick: function(){} // Callback after click
+    }).showToast();
+}
+
 function consultButton (producto) {
-    let addButton = document.querySelectorAll(".consulta")
-    addButton.forEach (button => {
-        button.onclick = (e) => {
-            const bikeId = e.currentTarget.id;
-            const selectedBike = producto.find(producto => producto.id == bikeId)
-            //mejorar con SweetAlert HTML TextBox
-            consultedBikes.push(selectedBike);
-            console.log(prompt("Ingrese datos bla bla bla"))
-            console.log(consultedBikes)
+    let consButton = document.querySelectorAll(".consulta")
+    consButton.forEach (button => {
+        button.onclick = async (e) => {
+            const motoId = e.currentTarget.id;
+            const motoElegida = producto.find(producto => producto.id == motoId)
+            const { value: formValues } = await Swal.fire({
+                title: "Elabore su consulta",
+                html: `
+                       <img id="swal-image" src="${motoElegida.img}" alt="img ${motoElegida.modelo}">
+                       <p>Precio: $${enPesos(motoElegida.precio)}</p>
+                       <p>Costo patentamiento: $${patentamiento(enPesos(motoElegida.precio))}</p>
+                       <label for="swal-input1">Nombre:</label>
+                       <input id="swal-input1" class="swal2-input" placeholder="Ingrese nombre"/>
+
+                       <label for="swal-input2">Apellido:</label>
+                       <input id="swal-input2" class="swal2-input" placeholder="Ingrese apellido"/>
+
+                       <label for="swal-input3">Teléfono:</label>
+                       <input id="swal-input3" class="swal2-input" placeholder="Ingrese solo números" maxlenght="20"/>
+
+                       <label for="swal-input4">Email:</label>
+                       <input id="swal-input4" class="swal2-input" placeholder="Ingrese email"/>
+
+                       <label for="swal-input5">Consulta:</label>
+                       <textarea id="swal-input5" class="swal2-textarea" placeholder="Ingrese su consulta"></textarea>
+                      `,
+                confirmButtonColor: "#333333",
+                confirmButtonText: "Enviar consulta",
+                showCancelButton: true,
+                cancelButtonColor: "#ff4343",
+                preConfirm: () => {
+                    const name = document.getElementById("swal-input1").value;
+                    const surname = document.getElementById("swal-input2").value;
+                    const telefono = document.getElementById("swal-input3").value;
+                    const email = document.getElementById("swal-input4").value;
+                    const consulta = document.getElementById("swal-input5").value;
+                    const formatoTexto = /^[a-zA-Z- ]+$/;
+                    const formatoTelefono = /^[0-9-]+$/;
+                    const formatoMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;     // Formato para validad el mail
+                    if (name == "" || !formatoTexto.test(name)) {
+                        Swal.showValidationMessage("Ingrese un nombre válido");
+                    } else if (surname == "" || !formatoTexto.test(surname)) {
+                        Swal.showValidationMessage("Ingrese un apellido válido");
+                    }else  if (telefono == "" && email == "") {
+                        Swal.showValidationMessage("Ingrese al menos su email");
+                    } else if (telefono != "" && !formatoTelefono.test(telefono)) {
+                        Swal.showValidationMessage("Ingrese solo números o deje este campo vacío");
+                    } else {
+                        if (!formatoMail.test(email)) {
+                            Swal.showValidationMessage("Ingrese un email válido");
+                        }
+                    }
+                    return [name, surname, telefono, email, consulta];
+                }
+            });
+            if (formValues) {
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    title: "Consulta enviada!",
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                const datosConsulta = JSON.stringify(formValues);
+                localStorage.setItem("datosConsulta", JSON.stringify(datosConsulta));       //En éste paso se enviaría la consulta al administrador
+            }
         }
     })
+}
+
+renderContacto = () => {
+    const datosContacto = {
+        nombre: "Martín Perez",
+        telefono: "11-2345-6789",
+        email: "mperez.motoshop@gmail.com"
+    };
+    mainMenu.innerHTML = "";
+    mainMenu.style.backgroundImage = "url('./images/fondos/bg-consulta.jpg')";
+    container.innerHTML = "";
+    const card = document.createElement ("div");
+    card.className = "contacto";
+    card.innerHTML = `<h2>${datosContacto.nombre}</h2>
+                      <h3>Tel.: ${datosContacto.telefono}</h3>
+                      <h3>Mail: ${datosContacto.email}</h3>`;
+    container.appendChild(card)
 }
 
 renderInicio();
